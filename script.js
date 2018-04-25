50b1c0634d966415929fb0c310d09c50dfc70a1a
Shellie Frai <saf159@miami.edu>
2018-04-25 13:24:55 -0400

    added new stuff

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ $(document).ready(function() {
   $(".container").hide();
 });
 
+$(document).ready(function() {
 $("#show").click(function() {
   $(".container").show();
 });
+});
 
+$(document).ready(function() {
 $("h2").hover(function() {
   $(".imghover").fadeOut(1000);
   $(".imghover").fadeIn(1000);
