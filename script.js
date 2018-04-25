f016acbcb850e85f681457040148fb2db131e712
Shellie Frai <saf159@miami.edu>
2018-04-25 12:58:53 -0400

    changes

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
   $("#title").html("<p>Strictly Interiors</p>");
 
-  $("#title").css("color", "blue");
+  $("#title").css("color", "pink");
+
+  $("#hide").click(function() {
+  $(".container").hide();
+});
+
+$("#show").click(function() {
+  $(".container").show();
+});
+
+$("h2").hover(function() {
+  $(".imghover").fadeOut(1000);
+  $(".imghover").fadeIn(1000);
+});
