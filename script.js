fb3f5fb38d6e21f4618014d33b8fa133e2067d24
Shellie Frai <saf159@miami.edu>
2018-06-15 10:05:16 -0400

    changes for ad portfolio

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  // $("#pagetitle").html("<p>Strictly Interiors</p>");
 
-  $("#pagetitle").css("color", "pink");
-
-  $("#hide").click(function() {
-  $(".container").hide();
-});
-});
-
-$(document).ready(function() {
-$("#show").click(function() {
-  $(".container").show();
-});
-});
-
-$(document).ready(function() {
-$("h2").hover(function() {
-  $(".imghover").fadeOut(1000);
-  $(".imghover").fadeIn(1000);
-});
-});
-
-$(document).ready(function(){
-  $("#home").click(function(){
-    $('#pagetitle').effect("shake", {times:4}, 1000);
-  });
-
-});
+  function myFunction(x) {
+      x.classList.toggle("change");
+  }
+// });
+//
+//   $("#hide").click(function() {
+//   $(".container").hide();
+// });
+// });
+//
+// $(document).ready(function() {
+// $("#show").click(function() {
+//   $(".container").show();
+// });
+// });
+//
+// $(document).ready(function() {
+// $("h2").hover(function() {
+//   $(".imghover").fadeOut(1000);
+//   $(".imghover").fadeIn(1000);
+// });
+// });
+//
+// $(document).ready(function(){
+//   $("#home").click(function(){
+//     $('#pagetitle').effect("shake", {times:4}, 1000);
+//   });
